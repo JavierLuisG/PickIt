8ed566be1d22da31b4a62af5e9986134425d5724
Javier Andrés Luis González <javierandresluisgonzalez@gmail.com>
2025-03-20 12:43:44 -0500

    feat: add HeroUI

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Providers } from "./providers";
 import { ContextGlobalProvider } from "../context/globalContext";
-import Navbar from "../components/navbar/page";
+import NavbarPage from "../components/navbar/page";
 import Footer from "../components/footer/Footer";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "PickIt",
@@ -27,11 +17,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body className="font-sans">
         <ContextGlobalProvider>
-          <Navbar />
-          {children}
-          <Footer />
+          <Providers>
+            <NavbarPage />
+            {children}
+            <Footer />
+          </Providers>
         </ContextGlobalProvider>
       </body>
     </html>
