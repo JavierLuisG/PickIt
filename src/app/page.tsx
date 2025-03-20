e45a3effa52e6ad00677940d38e474ddb5a039b6
Javier Andrés Luis González <javierandresluisgonzalez@gmail.com>
2025-03-20 01:20:22 -0500

    feat: install tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,10 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <>
-      <h1 className={styles.home_container}>Bienvenidos a PickIt</h1>
+      <h1 className="text-red-500 text-3xl font-bold">
+        ¡Hola, Tailwind funciona!
+        Bienvenido a Pick It
+      </h1>
     </>
   );
 }
