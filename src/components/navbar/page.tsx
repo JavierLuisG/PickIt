69b988a5ac7f5654619dc41563d95423f212211c
Javier Andrés Luis González <javierandresluisgonzalez@gmail.com>
2025-03-21 01:22:46 -0500

    style(navbar): new design with HeroUI

--- src/components/navbar/page.tsx
+++ src/components/navbar/page.tsx
@@ -1,43 +1,104 @@
 "use client";
 
+import React from "react";
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  NavbarMenuToggle,
+  NavbarMenu,
+  NavbarMenuItem,
+  Link,
+  Button,
+} from "@heroui/react";
 import styles from "../navbar/page.module.css";
-import Link from "next/link";
-import Image from "next/image";
 
-const Navbar = () => {
+export const PickitLogo = () => {
   return (
-    <>
-      <div className={styles.navbar_container}>
-        <Link href="/">
-          <Image
-            className={styles.logo_principal}
-            width={135}
-            height={45}
-            src="/images/pickit-logo-horizontal-color.png"
-            alt="logo"
-          />
-        </Link>
-        <div className={styles.options_container}>
-          <ul className={styles.options_navbar}>
-            <li><Link href="/contact">Nosotros</Link></li>
-            <li><Link href="/contact">Contáctanos</Link></li>
-            <li><Link href="/contact">Productos</Link></li>
-          </ul>
-          <div className={styles.icons_navbar}>
-            <Link className={styles.link_icons} href="/login">
-              <Image width={20} height={20} src="/profile.svg" alt="login" />
+    <Link href="/">
+      <img
+        className={styles.logo_navbar}
+        src="/images/pickit-logo-color.png"
+        alt="logo"
+      />
+    </Link>
+  );
+};
+
+const NavbarPage = () => {
+  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+
+  const menuItems = [
+    "Inicio",
+    "Ingresar",
+    "Registrate",
+    "Favoritos",
+    "Sobre nosotros",
+    "Contáctanos",
+    "Salir",
+  ];
+
+  return (
+    <Navbar onMenuOpenChange={setIsMenuOpen} isBordered>
+      <NavbarContent>
+        <NavbarBrand>
+          <PickitLogo />
+        </NavbarBrand>
+      </NavbarContent>
+
+      <NavbarContent className="hidden sm:flex gap-5" justify="center">
+        <NavbarItem>
+          <Link className="text-bold" color="foreground" href="#">
+            Contacto
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link color="foreground" href="#">
+            Productos
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link color="foreground" href="#">
+            Favoritos
+          </Link>
+        </NavbarItem>
+      </NavbarContent>
+      <NavbarContent className="hidden sm:flex gap-4" justify="end">
+        <NavbarItem>
+          <Button
+            className={styles.btn_navbar}
+            radius="full"
+            as={Link}
+            color="secondary"
+            href="/login"
+          >
+            Ingresar
+          </Button>
+        </NavbarItem>
+      </NavbarContent>
+      <NavbarMenuToggle
+        aria-label={isMenuOpen ? "Close menu" : "Open menu"}
+        className={`sm:hidden ${styles.menu_toggle}`}
+      />
+      <NavbarMenu>
+        {menuItems.map((item, index) => (
+          <NavbarMenuItem key={`${item}-${index}`}>
+            <Link
+              className="w-full"
+              color={
+                index === menuItems.length - 1 ? "danger" : "foreground"
+              }
+              href="#"
+              size="lg"
+            >
+              {item}
             </Link>
-            <Link className={styles.link_icons} href="/">
-              <Image width={25} height={25} src="/favorite.svg" alt="favs" />
-            </Link>
-            <Link className={styles.link_icons} href="/">
-              <Image width={25} height={25} src="/cart.svg" alt="favs" />
-            </Link>
-          </div>
-        </div>
-      </div>
-    </>
-  )
+          </NavbarMenuItem>
+        ))}
+      </NavbarMenu>
+    </Navbar>
+  );
 }
 
-export default Navbar;
+export default NavbarPage;
