59e69da20b3a6e2a6572b96942c1fbeedaed6aaf
Javier Andrés Luis González <javierandresluisgonzalez@gmail.com>
2025-03-31 14:38:23 -0500

    feat: create component Hero for Home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,12 @@
+"use client";
+
+import Hero from "../components/hero/page";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <>
-      <h1 className="text-red-500 text-3xl font-bold">
-        ¡Hola, Tailwind funciona!
-      </h1>
-      <h1 className={styles.home_container}>
-        Bienvenidos a PickIt
-      </h1>
-    </>
+    <div className={styles.home_container}>
+      <Hero />
+    </div>
   );
-}
+};
---
+++ src/components/hero/page.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import styles from "./page.module.css";
+import { Button, Link } from "@heroui/react";
+
+const Page: React.FC = () => {
+  return (
+    <section className={styles.hero_container}>
+      <div className={styles.hero_content}>
+        <p className={styles.subtitle}>Calidad en cada producto</p>
+        <h1 className={styles.title}>Descubre lo mejor de nuestros productos</h1>
+        <h2 className={styles.description}>
+          Encuentra productos de calidad a precios justos, pensados para ti.
+        </h2>
+        <Button
+          className={styles.hero_btn}
+          radius="full"
+          as={Link}
+          href={"/products"}
+          color="secondary"
+        >
+          Explorar productos
+        </Button>
+      </div>
+      <div className={styles.hero_image_container}>
+        <img src="/images/img_amigos_1.jpeg" className={styles.hero_img} />
+      </div>
+    </section>
+  )
+};
+
+export default Page;
