45860d0aa6bb6678e2d015ea860426e22dfd6f90
Javier Andrés Luis González <javierandresluisgonzalez@gmail.com>
2025-04-30 19:24:42 -0500

    fix: the modal issue on mobile devices

--- src/components/card/page.tsx
+++ src/components/card/page.tsx
@@ -9,6 +9,8 @@ import {
   Modal,
   ModalContent,
   Button,
+  ModalHeader,
+  ModalBody,
 } from "@heroui/react";
 
 type Producto = {
@@ -68,7 +70,26 @@ const CardComponent = ({ product }: CardComponentProps) => {
         radius="none"
       >
         <ModalContent>
-          {(onClose) => (<ModalCart product={product} onClose={onClose} />)}
+          <ModalHeader className={styles.title_modal}>{product.title}</ModalHeader>
+          <ModalBody>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam pulvinar risus non
+              risus hendrerit venenatis. Pellentesque sit amet hendrerit risus, sed porttitor
+              quam.
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam pulvinar risus non
+              risus hendrerit venenatis. Pellentesque sit amet hendrerit risus, sed porttitor
+              quam.
+            </p>
+            <p>
+              Magna exercitation reprehenderit magna aute tempor cupidatat consequat elit dolor
+              adipisicing. Mollit dolor eiusmod sunt ex incididunt cillum quis. Velit duis sit
+              officia eiusmod Lorem aliqua enim laboris do dolor eiusmod. Et mollit incididunt
+              nisi consectetur esse laborum eiusmod pariatur proident Lorem eiusmod et. Culpa
+              deserunt nostrud ad veniam.
+            </p>
+          </ModalBody>
         </ModalContent>
       </Modal>
     </>
