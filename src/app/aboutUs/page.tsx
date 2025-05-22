15ed3cee451b22b3411810a2144f4388f3a30326
Javier Andrés Luis González <javierandresluisgonzalez@gmail.com>
2025-05-22 12:03:34 -0500

    feat(products): add mock product data and refactor ProductsPage component

--- src/app/aboutUs/page.tsx
+++ src/app/aboutUs/page.tsx
@@ -1,11 +1,18 @@
 "use client";
 
 import styles from "./page.module.css";
+import HeroComponent from "../../components/hero/page";
+import AutCallToActionComponent from "../../components/authCallToAction/page";
+import HowToBuyComponent from "../../components/howToBuy/page";
+import ContactPage from "../contact/page";
 
 const AboutUsPage = () => {
   return (
     <div className={styles.aboutus_container}>
-      <h1>Bienvenidos a la pagina Sobre nosotros</h1>
+      <HeroComponent />
+      <AutCallToActionComponent />
+      <HowToBuyComponent />
+      <ContactPage />
     </div>
   );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,12 @@
 "use client";
 
-import HeroComponent from "../components/hero/page";
 import styles from "./page.module.css";
-import AutCallToActionComponent from "../components/authCallToAction/page";
-import HowToBuyComponent from "../components/howToBuy/page";
+import ProductsPage from "./products/page";
 
 const HomePage = () => {
   return (
     <div className={styles.home_container}>
-      <HeroComponent />
-      <AutCallToActionComponent />
-      <HowToBuyComponent />
+      <ProductsPage />
     </div>
   );
 };
